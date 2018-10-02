9645fa8cd923ada0808fbe3ea85b00b2596b1176
SpectraCoder <SpectraCoder@users.noreply.github.com>
2018-10-02 21:46:19 +0200

    Moved everything from MainPage to new Home page. This way there can be a navigation frame in MainPage to also show the About page.

---
+++ SubtitleRetimer/Home.xaml.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace SubtitleRetimer
+{
+    public sealed partial class Home : Page
+    {
+        public Home()
+        {
+            this.InitializeComponent();            
+            LoadAppbarIcon(ButtonAbout, "ms-appx:///Assets/IconInfoBlack.png", "ms-appx:///Assets/IconInfoWhite.png");
+
+        }
+
+        private async void ButtonLoadSrt_Click(object sender, RoutedEventArgs e)
+        {
+            bool succeeded = await Importer.LoadTextFile();
+
+            if (succeeded)
+            {
+                Dialogs.StatusMessage(TextBlockStatus, Colors.Green, "The file has loaded succesfully.");
+            }
+            else
+            {
+                Dialogs.StatusMessage(TextBlockStatus, Colors.Red, "No file loaded");
+            }
+
+        }
+
+        private async void ButtonExport_Click(object sender, RoutedEventArgs e)
+        {
+            if (Parameters.SubtitleList.Count != 0)
+            {
+                try
+                {
+
+                    if (ComboBoxMath.SelectedIndex == 0) //add
+                    {
+                        if (ComboBoxTime.SelectedIndex == 0) { Exporter.Add(Parameters.SubtitleList, int.Parse(TextBoxInput.Text)); } //add milliseconds
+                        if (ComboBoxTime.SelectedIndex == 1) { Exporter.Add(Parameters.SubtitleList, int.Parse(TextBoxInput.Text) * 1000); } //add seconds
+                        if (ComboBoxTime.SelectedIndex == 2) { Exporter.Add(Parameters.SubtitleList, int.Parse(TextBoxInput.Text) * 60000); } //add minutes
+                        if (ComboBoxTime.SelectedIndex == 3) { Exporter.Add(Parameters.SubtitleList, int.Parse(TextBoxInput.Text) * 3600000); } //add hours
+                    }
+
+                    if (ComboBoxMath.SelectedIndex == 1) //subtract
+                    {
+                        if (ComboBoxTime.SelectedIndex == 0) { Exporter.Subtract(Parameters.SubtitleList, int.Parse(TextBoxInput.Text)); } //subtract milliseconds
+                        if (ComboBoxTime.SelectedIndex == 1) { Exporter.Subtract(Parameters.SubtitleList, int.Parse(TextBoxInput.Text) * 1000); } //subtract seconds
+                        if (ComboBoxTime.SelectedIndex == 2) { Exporter.Subtract(Parameters.SubtitleList, int.Parse(TextBoxInput.Text) * 60000); } //subtract minutes
+                        if (ComboBoxTime.SelectedIndex == 3) { Exporter.Subtract(Parameters.SubtitleList, int.Parse(TextBoxInput.Text) * 3600000); } //subtract hours
+                    }
+
+                    bool succeeded = await Exporter.Export();
+
+                    if (succeeded)
+                    {
+                        Parameters.SubtitleList.Clear();
+                        Dialogs.StatusMessage(TextBlockStatus, Colors.Green, "The file was exported successfully.");
+                    }
+                    else
+                    {
+                        Parameters.SubtitleList.Clear();
+                        Dialogs.StatusMessage(TextBlockStatus, Colors.Red, "File saving canceled. Please import the file again before exporting.");
+                    }
+
+
+                }
+                catch (Exception)
+                {
+
+                    await Dialogs.ErrorDialog("Exporing aborted", "The value you entered isn't a numeric value or a whole number. Please try again.");
+                }
+            }
+
+            else
+            {
+                await Dialogs.ErrorDialog("Exporting aborted", "Please load a subtitle file first.");
+            }
+        }
+
+        public static void LoadIcon(Image image, string darkIconUri, string lightIconUri)
+        {
+            bool isLight = Application.Current.RequestedTheme == ApplicationTheme.Light;
+
+            BitmapImage bitmap = new BitmapImage();
+
+            if (isLight)
+            {
+                bitmap.UriSource = new Uri(darkIconUri);
+            }
+            else
+            {
+                bitmap.UriSource = new Uri(lightIconUri);
+            }
+
+            image.Source = bitmap;
+
+        }
+
+        public static void LoadAppbarIcon(AppBarButton button, string darkIconUri, string lightIconUri)
+        {
+            bool isLight = Application.Current.RequestedTheme == ApplicationTheme.Light;
+
+            BitmapIcon bitmap = new BitmapIcon();
+
+            if (isLight)
+            {
+                bitmap.UriSource = new Uri(darkIconUri);
+            }
+            else
+            {
+                bitmap.UriSource = new Uri(lightIconUri);
+            }
+
+            button.Icon = bitmap;
+
+        }
+
+        private void ButtonAbout_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            PageArea.Navigate(typeof(Assets.About));
+        }
+    }
+}
--- SubtitleRetimer/MainPage.xaml.cs
+++ SubtitleRetimer/MainPage.xaml.cs
@@ -22,76 +22,11 @@ namespace SubtitleRetimer{
         public MainPage()
         {
             this.InitializeComponent();
-            LoadAppIcon(AppIcon);
-        }
+            LoadIcon(AppIcon, "ms-appx:///Assets/AppIconBlack.png", "ms-appx:///Assets/AppIconWhite.png");            
+            PageArea.Navigate(typeof(Home));
+        }       
 
-        private async void ButtonLoadSrt_Click(object sender, RoutedEventArgs e)
-        {
-            bool succeeded = await Importer.LoadTextFile();
-
-            if (succeeded)
-            {
-                Dialogs.StatusMessage(TextBlockStatus, Colors.Green, "The file has loaded succesfully.");                
-            }
-            else
-            {
-                Dialogs.StatusMessage(TextBlockStatus, Colors.Red, "No file loaded");
-            }
-            
-        }        
-
-        private async void ButtonExport_Click(object sender, RoutedEventArgs e)
-        {
-            if (Parameters.SubtitleList.Count != 0)
-            {
-                try
-                {
-
-                    if (ComboBoxMath.SelectedIndex == 0) //add
-                    {
-                        if (ComboBoxTime.SelectedIndex == 0) { Exporter.Add(Parameters.SubtitleList, int.Parse(TextBoxInput.Text)); } //add milliseconds
-                        if (ComboBoxTime.SelectedIndex == 1) { Exporter.Add(Parameters.SubtitleList, int.Parse(TextBoxInput.Text) * 1000); } //add seconds
-                        if (ComboBoxTime.SelectedIndex == 2) { Exporter.Add(Parameters.SubtitleList, int.Parse(TextBoxInput.Text) * 60000); } //add minutes
-                        if (ComboBoxTime.SelectedIndex == 3) { Exporter.Add(Parameters.SubtitleList, int.Parse(TextBoxInput.Text) * 3600000); } //add hours
-                    }
-
-                    if (ComboBoxMath.SelectedIndex == 1) //subtract
-                    {
-                        if (ComboBoxTime.SelectedIndex == 0) { Exporter.Subtract(Parameters.SubtitleList, int.Parse(TextBoxInput.Text)); } //subtract milliseconds
-                        if (ComboBoxTime.SelectedIndex == 1) { Exporter.Subtract(Parameters.SubtitleList, int.Parse(TextBoxInput.Text) * 1000); } //subtract seconds
-                        if (ComboBoxTime.SelectedIndex == 2) { Exporter.Subtract(Parameters.SubtitleList, int.Parse(TextBoxInput.Text) * 60000); } //subtract minutes
-                        if (ComboBoxTime.SelectedIndex == 3) { Exporter.Subtract(Parameters.SubtitleList, int.Parse(TextBoxInput.Text) * 3600000); } //subtract hours
-                    }
-
-                    bool succeeded = await Exporter.Export();
-
-                    if (succeeded)
-                    {
-                        Parameters.SubtitleList.Clear();
-                        Dialogs.StatusMessage(TextBlockStatus, Colors.Green, "The file was exported successfully.");
-                    }
-                    else
-                    {
-                        Parameters.SubtitleList.Clear();
-                        Dialogs.StatusMessage(TextBlockStatus, Colors.Red, "File saving canceled. Please import the file again before exporting.");
-                    }
-                   
-
-                }
-                catch (Exception)
-                {
-
-                    await Dialogs.ErrorDialog("Exporing aborted", "The value you entered isn't a numeric value or a whole number. Please try again.");
-                }
-            }
-
-            else
-            {
-                await Dialogs.ErrorDialog("Exporting aborted", "Please load a subtitle file first.");
-            }
-        }
-
-        public static void LoadAppIcon(Image image)
+        public static void LoadIcon(Image image, string darkIconUri, string lightIconUri)
         {            
             bool isLight = Application.Current.RequestedTheme == ApplicationTheme.Light;
 
@@ -99,16 +34,35 @@ namespace SubtitleRetimer{
 
             if (isLight)
             {
-                bitmap.UriSource = new Uri("ms-appx:///Assets/AppIconBlack.png");
+                bitmap.UriSource = new Uri(darkIconUri);
             }
             else
             {
-                bitmap.UriSource = new Uri("ms-appx:///Assets/AppIconWhite.png");
+                bitmap.UriSource = new Uri(lightIconUri);
             }
 
             image.Source = bitmap;
             
-        }              
+        }
 
+        public static void LoadAppbarIcon(AppBarButton button, string darkIconUri, string lightIconUri)
+        {
+            bool isLight = Application.Current.RequestedTheme == ApplicationTheme.Light;
+
+            BitmapIcon bitmap = new BitmapIcon();
+
+            if (isLight)
+            {
+                bitmap.UriSource = new Uri(darkIconUri);
+            }
+            else
+            {
+                bitmap.UriSource = new Uri(lightIconUri);
+            }
+
+            button.Icon = bitmap;
+
+        }
+      
     }
 }
